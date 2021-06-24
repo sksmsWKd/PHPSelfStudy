e1560c53fb8b4376ebb9494a055567d533ba69f0
Song Jae Hyeon <0nocsace@gmail.com>
2021-06-24 17:37:39 +0900

    PHP 변수 지정

---
+++ variable1.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+<?php
+$a = 2;
+echo "php 변수 이름 지정시  앞에는 $ 표시.";
+echo "<br>\n 변수 a 에 1이 대입";
+echo "<br>\n";  
+
+echo $a;  #2출력
+echo "<br>\n";  
+echo $a+3; #5출력
+$b = $a*$a;
+echo "<br>\n";  
+echo $b; #아마도 4
+echo "<br>\n"; 
+$name = "카레에는 ";
+echo $name."토마토를 넣으면 맛있다";
+echo "<br>\n"; 
+echo $a.$name;
+
+
+/*
+주석이에요
+주석
+*/
+
+?>
+</body>
+</html>
