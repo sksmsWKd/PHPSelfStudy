a343985cb193c0b70cfcd8ab98c004c2a6ccf926
Song Jae Hyeon <0nocsace@gmail.com>
2021-06-29 18:45:34 +0900

    php 에서의 compare(var_dump)

---
+++ compare.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+<?php
+
+echo "1번."; 
+var_dump(1==2);
+#var_dump()어떠한형식의 데이터인지 화면에 표시하는 역할
+echo "<br>\n 2번";  
+
+
+var_dump("one"=="one");
+#비교연산자 문자열 비교가능 true / false 둘중 하나만 결과로
+echo "<br>\n 3번"; 
+
+
+
+var_dump(1!=2);
+#   ! 는 불린값 반대로  -- != 틀리다
+
+echo "<br>\n 4번";  
+
+
+
+
+var_dump(1!=1);
+#ㅇㅇ..
+
+echo "<br>\n 5번";  
+var_dump("one"!="twp");
+
+?>
+
+
+</body>
+</html>
